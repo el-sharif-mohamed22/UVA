8524077667c92b81b96269bee0aa1a2946d62837
el-sharif-mohamed22 <95565086+el-sharif-mohamed22@users.noreply.github.com>
2022-05-13 14:19:19 +0200

    Update solution.cpp

--- 10110 - Light, more light/solution.cpp
+++ 10110 - Light, more light/solution.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    unsigned int t;
+    while (cin >> t, t != 0) {
+        unsigned int root{static_cast<unsigned int>(sqrt(t))};
+        cout << (root * root == t ? "yes" : "no") << endl;
+    }
+}
